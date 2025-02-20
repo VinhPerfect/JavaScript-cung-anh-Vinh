c5950d20179c60adbec94ad14af14e26fa4cc73f
VinhPerfect <lqv.209@gmail.com>
2025-02-20 23:40:51 +0700

    JavaScript cung anh Vinh

--- JS cùng anh Vinh/baiTH2.2.js
+++ JS cùng anh Vinh/baiTH2.2.js
@@ -13,51 +13,59 @@ function daySo(a, b) {
     result += a + " ";
     a++;
   }
-  document.getElementById("result").innerText ="Day so tren la: " + result.trim();
+  document.getElementById("result").innerText =
+    "Day so tren la: " + result.trim();
 }
 //
 function print(a, b) {
   let result = "";
   while (a <= b) {
     if (a % 2 == 0) {
-      result += a + " ";      
+      result += a + " ";
     }
     a++;
   }
-  document.getElementById("print").innerText ="Cac so chan trong day la: " + result.trim();
+  document.getElementById("print").innerText =
+    "Cac so chan trong day la: " + result.trim();
 }
 //
-function oddSum(a, b){
-    let result = 0;
-    while (a <= b){
-        if (a % 2 != 0){
-            result += a;
-        }
-        a++;
+function oddSum(a, b) {
+  let result = 0;
+  while (a <= b) {
+    if (a % 2 != 0) {
+      result += a;
     }
-    document.getElementById("oddSum").innerText = "Tong cac so le trong day la: "+result;
+    a++;
+  }
+  document.getElementById("oddSum").innerText =
+    "Tong cac so le trong day la: " + result;
 }
 //
-function print3 (a, b){
+function print3(a, b) {
   let result = "";
-  while (a <= b){
-    if (a % 3 == 0){
+  while (a <= b) {
+    if (a % 3 == 0) {
       result += a + " ";
-    }    
+    }
     a++;
   }
   if (result.trim() == "") {
     document.getElementById("print3").innerText = "Không có số chia hết cho 3";
   } else {
-    document.getElementById("print3").innerText = "Cac so chia het cho 3 la: " + result.trim();
+    document.getElementById("print3").innerText =
+      "Cac so chia het cho 3 la: " + result.trim();
   }
 }
-// 
-function tiepTuc () {
-  if(confirm("Ban co muon tiep tuc khong")){
-    txt = "Ban da chon OK!";
+//
+function tiepTuc (){
+  if (confirm ("Ban co muon tiep tuc khong")){
+    let a = parseInt(prompt("Nhập số đầu tiên: "));
+    let b = parseInt(prompt("Nhập số cuối cùng: "));
+    daySo(a, b);
+    print(a, b);
+    oddSum(a, b);
+    print3(a, b);
   } else {
-    txt = "Thoat chuong trinh!";
+    alert("Thoat chuong trinh thanh cong");
   }
-  document.getElementById("tiepTuc").innerHTML = txt;
 }
